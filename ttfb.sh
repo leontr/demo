7883e5b82ab249a2c65bb1258abcc86a91833b40
Leonardo Trotti <leo@Leonardos-MacBook-Pro.local>
2018-12-02 17:47:55 +0000

    allow cache

--- ttfb.sh
+++ ttfb.sh
@@ -6,7 +6,6 @@
 # 2. seq 10 | xargs -Iz ./ttfb.sh "https://google.com"
 
 curl -o /dev/null \
-     -H 'Cache-Control: no-cache' \
      -s \
      -w "Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} \n" \
      $1
